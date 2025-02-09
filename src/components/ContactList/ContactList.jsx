828d2bd066f5ecacb25a7d13644cbc40f2bbf419
Ann_Ar0 <ann.artyuh90@gmail.com>
2025-02-09 15:10:18 +0200

    update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,25 +1,29 @@
 import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
-import { selectFilteredContacts } from "../../redux/filters/selectors";
+import { selectFilteredContacts } from "../../redux/contacts/selectors";
 
 const ContactList = () => {
-  const contacts = useSelector(selectFilteredContacts);
+  const contacts = useSelector(selectFilteredContacts) || [];
 
 
   return (
-    <div className={css.container}>
-      <ul className={css.list}>
-        {contacts.map((contact) => (
+  <div className={css.container}>
+    <ul className={css.list}>
+      {contacts.length > 0 ? (
+        contacts.map((contact) => (
           <Contact
             key={contact.id}
             id={contact.id}
             name={contact.name}
             number={contact.number}
           />
-        ))}
-      </ul>
-    </div>
+        ))
+      ) : (
+        <p>No contacts found</p>
+      )}
+    </ul>
+  </div>
   );
 };
 
